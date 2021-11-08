3a8a5dc8598f4bf6985768dec5dab6fe93b4f75e
Tahsan Nayem <80197475+Noob-Hacker71@users.noreply.github.com>
2021-11-08 21:51:26 +0400

    Add files via upload

--- Vic.py
+++ Vic.py
@@ -1,123 +1,181 @@
 ## import section
 import os, sys
+from time import sleep
 
 ##Requirement section
-os.system('apt install pyhton')
-print('python has been install ')
-os.system('apt install nano -y')
-print('nano has been isntalled ')
+try:
+  import python3
+except:
+  print("\033[1;33m")
+  os.system('apt install python3 -y')
+  print('python has been install ')
+  sleep(2)
+try:
+  import figlet
+except:
+  print("\033[1;34m")
+  os.system('apt install figlet -y')
+  print('figlet has been isntalled ')
+  sleep(2)
 
 ## owner info section
 
 os.system('clear')
 
-
-print('\033[1;94m ***************************************************************************************************')
-
-noob = ''' \033[1;91m ▄▄    ▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄    \033[1;92m ▄▄   ▄▄ ▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄   ▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄   
-\033[1;91m █  █  █ █       █       █  ▄    █  \033[1;92m █  █ █  █      █       █   █ █ █       █   ▄  █  
-\033[1;91m █   █▄█ █   ▄   █   ▄   █ █▄█   █  \033[1;92m █  █▄█  █  ▄   █       █   █▄█ █    ▄▄▄█  █ █ █  
-\033[1;91m █       █  █ █  █  █ █  █       █  \033[1;92m █       █ █▄█  █     ▄▄█      ▄█   █▄▄▄█   █▄▄█▄ 
-\033[1;91m █  ▄    █  █▄█  █  █▄█  █  ▄   █   \033[1;92m █   ▄   █      █    █  █     █▄█    ▄▄▄█    ▄▄  █
-\033[1;91m █ █ █   █       █       █ █▄█   █  \033[1;92m █  █ █  █  ▄   █    █▄▄█    ▄  █   █▄▄▄█   █  █ █
-\033[1;91m █▄█  █▄▄█▄▄▄▄▄▄▄█▄▄▄▄▄▄▄█▄▄▄▄▄▄▄█  \033[1;92m █▄▄█ █▄▄█▄█ █▄▄█▄▄▄▄▄▄▄█▄▄▄█ █▄█▄▄▄▄▄▄▄█▄▄▄█  █▄█
-'''
-print (noob)
-
-
-print('\033[1;92m NOOB-HACKER71.\nWe work to protect Bangladesh')
-
-print('\033[1;94m ***************************************************************************************************')
-print('\033[1;92m Owner: Tahsan Nayem')
-print('\033[1;92m Follow me : https://www.facebook.com/ntahsan.nayem')
-print('\033[1;94m ***********************************************************************************************************')
-
 ##userlogin section
-u = input('Enter your name : ')
-
-user = 'Tahsan Nayem'
-upas = 'THBD'
+usr= input('\033[1;32m\n\n[+] Enter your name : ')
+sleep(1)
+print ("\033[1;33m")
+os.system("figlet -f big Vic-Pass")
+sleep(1)
 while True:
-    loguser = input('\033[1;91m Enter Tool Username :')
-    if loguser == user:
-        print('\033[1;92m Currect User Name ')
-        logpas = input('\033[1;91m Enter Tool Password :')
-        if logpas == upas:
-            print("\033[1;92m Currect Password.\nWellcome to Noob-Hacker World ")
-            print(u)
+    loguser = input('\033[1;35m\n\n[+] Enter Tool Username :')
+    sleep(2)
+    if loguser == "NOOB-HACKER71":
+        print('\033[1;92m\n\n\t[+] Currect ')
+        logpas = input('\033[1;91m\n\n[+] Enter Tool Password :')
+        sleep(2)
+        if logpas == "THBD":
+            print("\033[1;32m\n\n\t[+] Access Granted\n\n[+] Wellcome to <<Noob-Hacker71>> World")
+            sleep(1)
+            print(usr)
             break
         else:
-            print("\033[1;91m Wrong Password")
+            print("\033[1;91m \n\n\t[+] Access Denied")
     else:
-        print('\033[1;91m Wrong UserName.\nTry Again sir')
-        print(u)
+        print('\033[1;91m\n\n\t[+] Wrong UserName\n\n[+] Try Again',usr,'sir')
+        
+os.system("clear")
+sleep(1)
+print('\033[1;94m *********************************************************************')
+sleep(1)
+noob = '''
+\033[1;92m     __            _          \033[1;92m                  _           \033[1;91m _____ _ 
+\033[1;92m  /\ \ \___   ___ | |__       \033[1;92m  /\  /\__ _  ___| | _____ _ _\033[1;91m|___  / |
+\033[1;92m /  \/ / _ \ / _ \| '_ \ \033[1;91m_____\033[1;92m / /_/ / _` |/ __| |/ / _ \ '__|\033[1;91m / /| |
+\033[1;92m/ /\  / (_) | (_) | |_) |\033[1;91m_____\033[1;92m/ __  / (_| | (__|   <  __/ |   \033[1;91m/ / | |
+\033[1;92m\_\ \/ \___/ \___/|_.__/      \033[1;92m\/ /_/ \__,_|\___|_|\_\___|_| \033[1;91m /_/  |_|
+'''
+print (noob)
+sleep(1)
+
+print("\033[1;94m\n[+] Termux Hacker BD {THBD}")
+sleep(1)
+print("\033[1;33m\n[+] We work to protect Bangladesh")
+sleep(1)
+print('\033[1;94m\n *********************************************************************')
+sleep(1)
+print('\033[1;92m[+] Author : Tahsan Nayem')
+sleep(1)
+print('\033[1;92m\n[+] Follow me : https://www.facebook.com/ntahsan.nayem')
+sleep(1)
+print('\033[1;94m**********************************************************************')
+
 ##raw_inputsection.....
-name = input('\033[1;91m Enter victim name :')
+name = input('\033[1;91m\n\n[+] Enter victim name :')
+sleep(1)
 
-surname = input('\033[1;92m Surname :')
-
-nickname = input('\033[1;93m Nickname :')
-
-gf = input('\033[1;94m Pertner name :')
-
-father = input("\033[1;95m Father's name :")
-
-mother = input("\033[1;96m Mother's name :")
-
-number = input('\033[1;92m Victim 1st 5 digit phone number :')
-numbr = input('\033[1;91m Enter full number :' )
-print(numbr[6:12])
-print('Wait until it says"Password.txt file Done "')
-
-
+surname = input('\033[1;92m\n\n[+] Surname :')
+sleep(1)
+nickname = input('\033[1;93m\n\n[+] Nickname :')
+sleep(1)
+gf = input('\033[1;94m\n\n[+] Pertner name :')
+sleep(1)
+father = input("\033[1;95m\n\n[+] Father's name :")
+sleep(1)
+mother = input("\033[1;96m\n\n[+] Mother's name :")
+sleep(1)
+number = input('\033[1;92m\n\n[+] Victim 1st 6 digit phone number :')
+sleep(1)
+numbr = input('\033[1;91m\n\n[+] Enter full number :' )
+sleep(1)
+print('\033[1;32m\n\n[+] Wait until it says\033[1;36m"Password.txt" \033[1;32mfile Done ')
+sleep(1)
 
 
 ##function section
+file=open("password.txt","a")
+
+if numbr=="":
+  pass
+else:
+  jj= (numbr[6:])
+  file.write(jj+'\n')
+  file.write(numbr+'\n')
+if number=="":
+  pass
+else:
+  file.write(number+'\n')
 
 
-for var in range(0, 10000000):
+for var in range(0, 100):
+  
+  if name=="":
+    pass
+  else:
     na = name+str(var)
-    #print(na)
-    aa = surname+str(var)
-    #print(aa)
-    bb = nickname+str(var)
-    #print(bb)
-    cc = gf+str(var)
-    #print(cc)
-    dd = father+str(var)
-    #print(dd)
-    ee = mother+str(var)
-    #print(ee)
-    ff = name+surname+str(var)
-    #print(ff)
-    gg = name+gf+str(var)
-    #print(gg)
-    hh = number+str(var)
-    #print (hh)
-
-    jj = numbr+str(var)
-    #print (jj)
-    
-
-    file = open('password.txt', 'a')
     file.write(na+'\n')
+    #print(na)
+  if surname=="":
+    pass
+  else:
+    kk=name+surname+str(var)
+    aa = surname+str(var)
     file.write(aa+'\n')
+    file.write(kk+'\n')
+    #print(aa)
+  if nickname=="":
+    pass
+  else:
+    bb = nickname+str(var)
     file.write(bb+'\n')
+    #print(bb)
+  if gf=="":
+    pass
+  else:
+    cc = gf+str(var)
     file.write(cc+'\n')
+    #print(cc)
+  if father=="":
+    pass
+  else:
+    dd = father+str(var)
     file.write(dd+'\n')
+    #print(dd)
+  if mother=="":
+    pass
+  else:
+    ee = mother+str(var)
     file.write(ee+'\n')
+    #print(ee)
+  if name or surname=="":
+    pass
+  else:
+    ff = name+surname+str(var)
     file.write(ff+'\n')
+    #print(ff)
+  if name or gf =="":
+    pass
+  else:
+    gg = name+gf+str(var)
     file.write(gg+'\n')
-    file.write(hh+'\n')
-    file.write(jj+'\n')
+    #print(gg)
 
 
     file.close()
-print('Password.txt file done')
-ye = 'yes'
-n = 'no'
-see = input('\033[1;92m Do You want see the passwords : ')
-if see == ye:
-    os.system('cat password.txt')
-elif see == n:
-    print('Thank You For Using This Tool',u,'Sir')
+print('\n\n[+] Password.txt file done')
+sleep(1)
+see = input('\033[1;95m\n\n[+] Do You want see the passwords [y/n] :  ')
+sleep(2)
+y="y"
+if see==y:
+  os.system('cat password.txt')
+else:
+  print("\033[1;35m\n\n[+] Run 'cat password.txt' For see password manually ")
+sleep(1)
+run=input("\033[1;33m\n\n[+]Do you Want to run again [y/n] : ")
+if run==y:
+  os.system("python3 Task.py")
+else:
+  print("\033[1;36m\n\nThank you for using me.",usr,"sir")
+  exit()
